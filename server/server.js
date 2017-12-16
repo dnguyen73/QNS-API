7c7e700488332e429e12cded1fb0fa6b8d927c6f
Duy Nguyen <dnguyen73@csc.com>
2017-12-16 11:13:15 +0700

    enable ssl and configure for production mode

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 var path = require('path');
 
+var http = require('http');
+var https = require('https');
+var sslConfig = require('./ssl-config');
+
 var app = module.exports = loopback();
 
-app.use(loopback.token({  
+app.use(loopback.token({
   cookies: ['access_token'],
   headers: ['access_token', 'X-Access-Token']
   //additional keys (check docs for more info)
@@ -15,22 +19,47 @@ app.use(loopback.token({
 app.set('view engine', 'ejs'); // LoopBack comes with EJS out-of-box
 app.set('views', path.resolve(__dirname, 'views'));
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+app.start = function (httpOnly) {
+  if (httpOnly === undefined) {
+    httpOnly = process.env.HTTP;
+  }
+  var server = null;
+  if (!httpOnly) {
+    var options = {
+      key: sslConfig.privateKey,
+      cert: sslConfig.certificate,
+    };
+    server = https.createServer(options, app);
+  } else {
+    server = http.createServer(app);
+  }
+
+  server.listen(app.get('port'), function() {
+    var baseUrl = (httpOnly ? 'http://' : 'https://') + app.get('host') + ':' + app.get('port');
+    app.emit('started', baseUrl);
+    console.log('LoopBack server listening @ %s%s', baseUrl, '/');
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
+  return server;
+
+  // start the web server
+  // return app.listen(function () {
+  //   app.emit('started');
+  //   var baseUrl = app.get('url').replace(/\/$/, '');
+  //   console.log('Web server listening at: %s', baseUrl);
+  //   if (app.get('loopback-component-explorer')) {
+  //     var explorerPath = app.get('loopback-component-explorer').mountPath;
+  //     console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+  //   }
+  // });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
